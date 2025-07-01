a0b72b067ec54673012fb7bc36b5cfc696cf45a1
itssTim <86143692+itssTim@users.noreply.github.com>
2025-06-30 17:27:03 -0700

    Adding FreeCodeCamp Basic Javascript Lab Solutions. Working with variables, strings, booleans and numbers

---
+++ sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "loud"
+let noun = "You"
+let verb = "are"
+let place = "library"
+let adjective2 = "crowded"
+let noun2 = "kid"
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "b"
+noun = "a"
+verb = "e"
+place = "z"
+adjective2 = "cramped"
+noun2 = "s"
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
---
+++ triviabot.js
@@ -0,0 +1,24 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "F";
+let botLocation = "H";
+let favoriteLanguage = "Js";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "My favorte language is " + favoriteLanguage;
+
+console.log(codingFact);
+
+
+codingFact = favoriteLanguage + "rocks!";
+
+console.log(codingFact);
+
+codingFact = "why is " + favoriteLanguage + " so repetitive?";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
